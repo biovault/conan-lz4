7a99f0934ee5f9b68a736b56f0901900a67d235e
Baldur van Lew <b.van_lew@lumc.nl>
2025-03-21 16:32:31 +0100

    Force the C standard

--- conanfile.py
+++ conanfile.py
@@ -78,6 +78,7 @@ endif()""")
         tc.variables["CMAKE_CXX_STANDARD"] = "17"
         if self.settings.os == "Linux":
             tc.variables["CMAKE_C_STANDARD"] = "17"
+            tc.variables["CMAKE_C_STANDARD_REQUIRED"] = "ON"
         tc.variables["BUILD_STATIC_LIBS"] = "True"
 
         return tc
