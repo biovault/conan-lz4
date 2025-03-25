fc7548e2a4e87343ee16694d1a970e54d33d8a91
Baldur van Lew <b.van_lew@lumc.nl>
2025-03-25 09:19:33 +0100

    Disable use of ISO23
    Using compile  flags -U_ISOC23_SOURCE -D_DEFAULT_SOURCE

--- conanfile.py
+++ conanfile.py
@@ -79,7 +79,7 @@ endif()""")
         # Restrict the C standard to 17 to avoid the issues with
         # isoc23 when consuming this in manylinux for nptsne building
         if self.settings.os == "Linux" and self.settings.compiler.version == "14":
-            tc.variables["CMAKE_C_FLAGS"] = "${CMAKE_C_FLAGS} -m64 -std=c99"
+            tc.variables["CMAKE_C_FLAGS"] = "${CMAKE_C_FLAGS} -m64 -std=c99 -U_ISOC23_SOURCE -D_DEFAULT_SOURCE"
         #    tc.variables["CMAKE_C_STANDARD"] = "17"
         #    tc.variables["CMAKE_C_STANDARD_REQUIRED"] = "ON"
         #    tc.variables["CMAKE_C_EXTENSIONS"] = "OFF"
